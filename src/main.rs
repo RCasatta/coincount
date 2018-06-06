a928de63bd96f96b479ecde85bced43306047961
Riccardo Casatta <riccardo@casatta.it>
2018-06-06 12:41:36 +0200

    reduce number of observed size, using clear on the set instead of reallocating

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ impl Counter {
 
     pub fn count(&mut self, line: &Line) {
         if line.height % self.size == 0 {
-            self.set = HashSet::new();
+            self.set.clear();
         }
 
         if line.input {
@@ -50,7 +50,7 @@ impl Counter {
 }
 
 fn main() {
-    let sizes = [2u32,4,8,16,32,64,128,256,512,1024,2016,2048,4096,8192,16384,32768,65536];
+    let sizes = [2u32,4,8,16,32,64,144,256,1024,4096,16384,65536];
     let mut counters : Vec<Counter> = Vec::new();
     for size in sizes.iter() {
         counters.push(Counter::new(size.clone()));
